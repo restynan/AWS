6bd7defa0119562b1a02d5aa1325cba9cc69d41b
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-25 21:28:07 -0500

    geting content from the bucket

--- demoaws/src/main/java/com/example/demoaws/DemoawsApplication.java
+++ demoaws/src/main/java/com/example/demoaws/DemoawsApplication.java
@@ -5,40 +5,62 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
-import org.springframework.beans.factory.annotation.Value;
+
+
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.example.demoaws.models.DemoUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
+
 @SpringBootApplication
 public class DemoawsApplication {
-    @Value("${access.key}")
-   private static String accesskey;
-    @Value("${secret.key}")
-    private static String secretkey;
+
+    private static Logger logger = LoggerFactory.getLogger(DemoawsApplication.class);
+    private static String SOURCE_BUCKET_NAME = "testdemo13";
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoawsApplication.class, args);
 
 
-      //  final AmazonS3 s3 =AmazonS3ClientBuilder.standard().build();
+        //  final AmazonS3 s3 =AmazonS3ClientBuilder.standard().build();
 
-        BasicAWSCredentials cred= new BasicAWSCredentials("AKIAXXBMI6WTYYQCZUG6","qNkFyF8tX+0rICPCxepgY/S7C16lyA8Axn3Q8h2n");
-        AmazonS3 s3 =AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(cred)).build();
+        BasicAWSCredentials cred = new BasicAWSCredentials("AKIAXXBMI6WTYWZX444R", "TOnO0me/DkouRA2ABPARLasCTdeNKqbh2qDroYak");
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(cred)).build();
+
+        //Listing all buckets
+        List<Bucket> buckets = s3.listBuckets();
+
+        //geting content from bucket
+        S3Object object = s3.getObject(SOURCE_BUCKET_NAME, "userDemo.txt");
+        S3ObjectInputStream stream = object.getObjectContent();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            DemoUser user = mapper.readValue(stream, DemoUser.class);
+            System.out.println(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
 
-       List<Bucket> buckets=s3.listBuckets();
-        buckets.stream().forEach(bucket->{
+        //interating through the bucket
+        logger.info("printing bucket details");
+        buckets.stream().forEach(bucket -> {
 
-            System.out.println("Bucket Name: "+bucket.getName() +", Bucket owner:  "+bucket.getOwner().getDisplayName()+
-                        " ,Bucket creation date:  "+bucket.getCreationDate());
+                    logger.info("Bucket Name: " + bucket.getName() + ", Bucket owner:  " + bucket.getOwner().getDisplayName() +
+                            " ,Bucket creation date:  " + bucket.getCreationDate());
                 }
 
 
-
-                );
-
+        );
 
 
     }
---
+++ demoaws/src/main/java/com/example/demoaws/models/DemoUser.java
@@ -0,0 +1,41 @@
+package com.example.demoaws.models;
+
+public class DemoUser {
+
+    private String firstName;
+    private String secondName;
+     private int age       ;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "DemoUser{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
