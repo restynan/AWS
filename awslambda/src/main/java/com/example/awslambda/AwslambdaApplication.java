0228ad7b14fe0456c945b5739781992f6ce27d95
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-27 17:49:52 -0500

    lambda

---
+++ awslambda/src/main/java/com/example/awslambda/AwslambdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.awslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwslambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwslambdaApplication.class, args);
+    }
+
+}
--- awslambda/src/main/java/com/example/awslambda/service/LambdaWithS3.java
+++ awslambda/src/main/java/com/example/awslambda/service/LambdaWithS3.java
@@ -1,4 +1,23 @@
 package com.example.awslambda.service;
 
-public class LambdaWithS3 {
+import com.amazonaws.handlers.RequestHandler2;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Event;
+
+public class LambdaWithS3 implements RequestHandler<S3Event,String> {
+    private AmazonS3 s3  = AmazonS3ClientBuilder.standard().build();
+    private final static String DESTINATION_BUCKET_NAME = "destinationbucket12";
+    @Override
+    public String handleRequest(S3Event s3Event, Context context) {
+
+
+
+
+
+
+        return null;
+    }
 }
